53a926b3816069c7fcbf0ea4f5e904751ed9b4c1
thaliabernalepe <thaliabernal@unicauca.edu.co>
2026-09-09 22:44:17 -0500

    actualizacion app.js y pruebas postman

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
+import deckRoutes from './routes/deckRoutes.js';
+import tarjetaRoutes from './routes/tarjetaRoutes.js';
 
 const app = express();
 
@@ -8,5 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', routes);
+
+// Rutas de mazos y tarjetas
+app.use('/api/v1/decks', deckRoutes);
+app.use('/api/v1/tarjetas', tarjetaRoutes);
 
 export default app;
